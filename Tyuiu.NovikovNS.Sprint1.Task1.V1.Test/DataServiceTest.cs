8e0398645f902f9393eb987611708efa8c0b4501
NovikovNikita72 <145142247+NovikovNikita72@users.noreply.github.com>
2023-09-20 23:40:37 +0500

    Task 1

---
+++ Tyuiu.NovikovNS.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NovikovNS.Sprint1.Task1.V1.Lib;
+
+namespace Tyuiu.NovikovNS.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3.0;
+            double y = 1.0;
+            double a = 1.0;
+            var res = ds.Calculate(x, y, a);
+            Assert.AreEqual(7, res);
+        }
+    }
+}
